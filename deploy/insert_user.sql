6ac21932ab37937227608026d1b085d6d1012180
Mathieu Tourte <tourte.mathieu@gmail.com>
2022-09-12 12:59:25 +0200

    initial commit

---
+++ deploy/insert_user.sql
@@ -0,0 +1,15 @@
+-- Deploy flipr:insert_user to pg
+-- requires: users
+-- requires: appschema
+-- requires: pgcrypto
+
+BEGIN;
+
+CREATE OR REPLACE FUNCTION flipr.insert_user(
+    nickname TEXT,
+    password TEXT
+) RETURNS VOID LANGUAGE SQL SECURITY DEFINER AS $$
+    INSERT INTO flipr.users values($1, crypt($2, gen_salt('md5')));
+$$;
+
+COMMIT;
